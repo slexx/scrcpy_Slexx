a72a4fd6f5744a873e98bb8e5e7bdda9cf8a6764
AlexM <1007808@student.sae.edu.au>
2019-09-27 00:29:14 +1000

    still some errors. Works for USB though!

--- PS_RUN_SCRCPY.ps1
+++ PS_RUN_SCRCPY.ps1
@@ -20,13 +20,14 @@ Creator: SlexxSAE
 
 #This func will ask for user input and check if the $input matches any of the values in $array, otherwise calling itself again
 function user_input{
-$array = "w","s","d","x","t","l" #each option added to the switch needs to be added here!
+$array = "w","s","d","x","t","l","k" #each option added to the switch needs to be added here!
 
 $input = Read-Host 'OPTIONS:
-(S) Start
+(K) Kill ADB Server (Step 1 if you dont know what you are doing..)
+(D) Devices List
 (W) Reset ADB to Wifi(TCPIP)
-(L) Reset ADB to Local(USB) 
-(D) Devices List 
+(L) Reset ADB to Local(USB)
+(S) Start
 (X) Exit 
 (T) Test 
 >> Your Input?'
@@ -44,32 +45,65 @@ switch($input)
 {
     w {
 		.\adb kill-server
-		Read-Host -prompt "ADB Server killed... 1/3"
+		Write-Host "ADB Server killed, Next step in 3..2..1.."		
+		Start-Sleep 3
+		#Read-Host -prompt "ADB Server killed... 1/3"
+		
 		.\adb usb #Reset the ADB to USB mode
-		Read-Host -prompt "ADB Set to USB... 2/3"
+		Write-Host "ADB Set to USB mode.."
+		Start-Sleep 2
+		#Read-Host -prompt "ADB Set to USB... 2/3"	
+		
 		.\adb tcpip 5555 #Re-Set to TCPIP MOde, set port to 5555
-		Read-Host -prompt "ADB TCPIP Set >> DISCONNECT THE DEVICE FROM USB.. << 3/3"
+		Write-Host "ADB Set back to TCPIP:5555"
+		Start-Sleep 2
+		#Read-Host -prompt "ADB TCPIP Set >> DISCONNECT THE DEVICE FROM USB.. << 3/3"
+		
 		.\adb connect 192.168.0.30:5555 #connect to ADB
-		user_input
+		Write-Host "Connecting to ADB Remote..."
+		Start-Sleep 1
+		user_input		
 	}
+	
 	l {
 		.\adb kill-server
 		Read-Host -prompt "ADB Server killed... 1/3"
 		.\adb usb #Reset the ADB to USB mode
 		Read-Host -prompt "ADB Set to USB... 2/2"
 		user_input
+		
 	}
+	
     s {
-	.\scrcpy -b 2M #run screen Copy for WIFI settings (reduced BW)
+		.\scrcpy #run screen Copy for WIFI settings (reduced BW)
 	}
+	
     d {
 		.\adb devices #check if the device is detected first
+		Start-Sleep 2
+		Write-Host "Checking Connected Device(s).."
+		user_input
+		
 	}
+	
+	k {
+		.\adb kill-server
+		Write-Host " Killing ADB Server.."
+		Start-Sleep 2
+		user_input
+		
+	}
+	
 	x {exit}
+	
 	t {
-	'Test Complete..'
-	pause	
+		'Test Complete..'
+		user_input			
 	}
-    default {'Try again..'}
+	
+		default {
+		'Try again..'
+		clv -Name "input"
+	}
 }
 #pause
