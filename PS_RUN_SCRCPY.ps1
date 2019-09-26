e6d00d850f6e2614e69322ba473e2f78e7a802fb
AlexM <1007808@student.sae.edu.au>
2019-09-27 01:19:18 +1000

    more fiddling, havent really improved much! need to conver current switch statement into functions called rather than commands directly in the switch

--- PS_RUN_SCRCPY.ps1
+++ PS_RUN_SCRCPY.ps1
@@ -18,28 +18,67 @@ while also being able to run scrcpy or check devices
 Creator: SlexxSAE
 #>
 
+
+
 #This func will ask for user input and check if the $input matches any of the values in $array, otherwise calling itself again
 function user_input{
-$array = "w","s","d","x","t","l","k" #each option added to the switch needs to be added here!
+	$array = "w","s","d","x","t","l","k" #each option added to the switch needs to be added here!
 
-$input = Read-Host 'OPTIONS:
-(K) Kill ADB Server (Step 1 if you dont know what you are doing..)
-(D) Devices List
-(W) Reset ADB to Wifi(TCPIP)
-(L) Reset ADB to Local(USB)
-(S) Start
-(X) Exit 
-(T) Test 
->> Your Input?'
-if($array -notcontains $input){
-	Write-Host "Not a correct answer, Try again..."
-	user_input #calls this function if the $input didnt match $array
+	$input = Read-Host 'OPTIONS:
+	(K) Kill ADB Server (Step 1 if you dont know what you are doing..)
+	(D) Devices List
+	(W) Reset ADB to Wifi(TCPIP)
+	(L) Reset ADB to Local(USB)
+	(S) Start
+	(X) Exit 
+	(T) Test 
+	>> Your Input?'
+	if($array -notcontains $input){
+		Write-Host "Not a correct answer, Try again..."
+		user_input #calls this function if the $input didnt match $array
+	}
+
+	return $input
 }
 
-return $input
+function Get_Devices{
+	$devices = .\adb devices
+	return $devices	
 }
 
+function Kill_ADB{
+	.\adb kill-server
+	Start-Sleep 1
+	Write-Host 'ADB Killed..'
+
+}
+
+function Wifi_ADB{
+.\adb kill-server
+		Write-Host "ADB Server killed, Next step in 3..2..1.."		
+		Start-Sleep 1
+		#Read-Host -prompt "ADB Server killed... 1/3"
+		
+		.\adb usb #Reset the ADB to USB mode
+		Write-Host "ADB Set to USB mode.."
+		Start-Sleep 1
+		#Read-Host -prompt "ADB Set to USB... 2/3"	
+		
+		.\adb tcpip 5555 #Re-Set to TCPIP MOde, set port to 5555
+		Write-Host "ADB Set back to TCPIP:5555"
+		Start-Sleep 1
+		#Read-Host -prompt "ADB TCPIP Set >> DISCONNECT THE DEVICE FROM USB.. << 3/3"
+		
+		.\adb connect 192.168.0.30:5555 #connect to ADB
+		Write-Host "Connecting to ADB Remote..."
+		Start-Sleep 1
+
+}
+
+
+#VARIABLES
 $input = user_input
+#$client = $null
 
 switch($input)
 {
@@ -61,17 +100,14 @@ switch($input)
 		
 		.\adb connect 192.168.0.30:5555 #connect to ADB
 		Write-Host "Connecting to ADB Remote..."
-		Start-Sleep 1
-		user_input		
+		Start-Sleep 1		
 	}
 	
 	l {
 		.\adb kill-server
 		Read-Host -prompt "ADB Server killed... 1/2"
 		.\adb usb #Reset the ADB to USB mode
-		Read-Host -prompt "ADB Set to USB... 2/2"
-		user_input
-		
+		Read-Host -prompt "ADB Set to USB... 2/2"		
 	}
 	
     s {
@@ -82,28 +118,26 @@ switch($input)
 		.\adb devices #check if the device is detected first
 		Start-Sleep 2
 		Write-Host "Checking Connected Device(s).."
-		user_input
-		
+		user_input		
 	}
 	
 	k {
-		.\adb kill-server
-		Write-Host " Killing ADB Server.."
-		Start-Sleep 2
-		user_input
-		
+		Kill_ADB
+		user_input		
 	}
 	
 	x {exit}
 	
-	t {
-		'Test Complete..'
-		user_input			
+	t {		
+		Get_Devices
+		Start-Sleep 2
+		user_input	
+		'Test Complete..'		
 	}
 	
-		default {
-		'Try again..'
-		clv -Name "input"
+	default {
+		user_input
 	}
 }
+Read-Host ' Something went wrong, Restart the script! '
 #pause
